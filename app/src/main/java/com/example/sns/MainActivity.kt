9531b1c9e7fd8fc537d2beb6a54b5c4f3b8181bc
minji05 <smj95133@likelion.org>
2023-10-15 17:59:24 +0900

    Design : MainActivity 디자인(1)

--- app/src/main/java/com/example/sns/MainActivity.kt
+++ app/src/main/java/com/example/sns/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.sns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sns.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding. imageView4.clipToOutline = true
+
     }
 }
